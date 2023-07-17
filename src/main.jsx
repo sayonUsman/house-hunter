4df1d835c00393d2f768669007750a40e2584afa
Sayon Usman <sayonusman.ma@gmail.com>
2023-07-17 23:49:01 +0300

    Sign up page has been added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import SignUp from "./pages/SignUp/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">House Hunter</h1>
+    <SignUp></SignUp>
   </React.StrictMode>
 );
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,162 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const SignUp = () => {
+  const [role, setRole] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const manageRole = (event) => {
+    setRole(`${event.target.value}`);
+  };
+
+  const onSubmit = (newUserData) => {
+    setErrorMessage("");
+    if (role === "") {
+      setErrorMessage("Please Select Your Role");
+      return;
+    }
+
+    console.log(newUserData, role);
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card flex-shrink-0 w-72 sm:w-96 shadow-2xl bg-base-100">
+          <div className="card-body">
+            <select
+              onChange={(event) => manageRole(event)}
+              className="select select-bordered w-full"
+              required
+            >
+              <option disabled selected>
+                Kindly Select Your Role
+              </option>
+              <option value="House Owner">House Owner</option>
+              <option value="House Renter">House Renter</option>
+            </select>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Full Name</span>
+                </label>
+
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  className="input input-bordered"
+                  {...register("name", { required: true })}
+                />
+
+                {errors.name && (
+                  <span className="text-red-500 pt-1 pl-1">
+                    Full Name is required.
+                  </span>
+                )}
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="input input-bordered"
+                  {...register("email", { required: true })}
+                />
+
+                {errors.email && (
+                  <span className="text-red-500 pt-1 pl-1">
+                    Email is required.
+                  </span>
+                )}
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Phone</span>
+                </label>
+
+                <input
+                  type="tel"
+                  placeholder="+8801XXXXXXXXX"
+                  className="input input-bordered"
+                  {...register("phone", {
+                    required: true,
+                    pattern: /[+]{1}[8]{2}[0]{1}[1]{1}[0-9]{9}/,
+                  })}
+                />
+
+                {errors.phone?.type === "required" && (
+                  <span className="text-red-500 pt-1 pl-1">
+                    Phone number is required.
+                  </span>
+                )}
+
+                {errors.phone?.type === "pattern" && (
+                  <span className="text-red-500 pt-1 pl-1">
+                    Only Bangladeshi phone numbers are allowed.
+                  </span>
+                )}
+              </div>
+
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="input input-bordered"
+                  {...register("password", { required: true, minLength: 6 })}
+                />
+
+                {errors.password?.type === "required" && (
+                  <span className="text-red-500 pt-1 pl-1">
+                    Password is required.
+                  </span>
+                )}
+
+                {errors.password?.type === "minLength" && (
+                  <span className="text-red-500 pt-1 pl-1">
+                    Password must be six characters.
+                  </span>
+                )}
+              </div>
+
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Sign Up"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {errorMessage && (
+        <div className="toast toast-end">
+          <div className="alert alert-error">
+            <div>
+              <span>{errorMessage}</span>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SignUp;
