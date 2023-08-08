9e2f4fc6d46a1c70ad8a37ed1a94c2bf19c88f94
Sayon Usman <sayonusman.ma@gmail.com>
2023-08-08 13:00:24 +0300

    Added footer

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar/Navbar";
+import Footer from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div className="container mx-auto">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
