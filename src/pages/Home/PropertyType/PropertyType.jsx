8b514f9d3ff78efe8ede570487c7dbcb3765742c
Sayon Usman <sayonusman.ma@gmail.com>
2023-08-27 15:08:08 +0300

    Added framer motion in property type section

--- src/pages/Home/PropertyType/PropertyType.jsx
+++ src/pages/Home/PropertyType/PropertyType.jsx
@@ -4,6 +4,7 @@ import family from "../../../assets/houseType/family.jpg";
 import penthouse from "../../../assets/houseType/penthouse.jpg";
 import resthouse from "../../../assets/houseType/resthouse.jpg";
 import villa from "../../../assets/houseType/villa.jpg";
+import { motion } from "framer-motion";
 
 const PropertyType = () => {
   return (
@@ -15,53 +16,65 @@ const PropertyType = () => {
       <div className="flex flex-wrap lg:-m-2">
         <div className="flex w-full lg:w-1/2 flex-wrap flex-col lg:flex-row">
           <div className="w-full lg:w-1/2 pt-2 lg:p-2">
-            <img
-              alt="Gallery"
-              className="block h-full w-full object-cover object-center"
-              src={evergreen}
-            />
+            <motion.div whileHover={{ scale: 0.9 }} className="h-full w-full">
+              <img
+                alt="Gallery"
+                className="block h-full w-full object-cover object-center"
+                src={evergreen}
+              />
+            </motion.div>
           </div>
 
           <div className="w-full lg:w-1/2 pt-2 lg:p-2">
-            <img
-              alt="gallery"
-              className="block h-full w-full object-cover object-center"
-              src={resthouse}
-            />
+            <motion.div whileHover={{ scale: 0.9 }} className="h-full w-full">
+              <img
+                alt="gallery"
+                className="block h-full w-full object-cover object-center"
+                src={resthouse}
+              />
+            </motion.div>
           </div>
 
           <div className="w-full pt-2 lg:p-2">
-            <img
-              alt="gallery"
-              className="block h-full w-full object-cover object-center"
-              src={family}
-            />
+            <motion.div whileHover={{ scale: 0.9 }} className="h-full w-full">
+              <img
+                alt="gallery"
+                className="block h-full w-full object-cover object-center"
+                src={family}
+              />
+            </motion.div>
           </div>
         </div>
 
         <div className="flex w-full lg:w-1/2 flex-wrap flex-col lg:flex-row">
           <div className="w-full pt-2 lg:p-2">
-            <img
-              alt="gallery"
-              className="block h-full w-full object-cover object-center"
-              src={apartment}
-            />
+            <motion.div whileHover={{ scale: 0.9 }} className="h-full w-full">
+              <img
+                alt="gallery"
+                className="block h-full w-full object-cover object-center"
+                src={apartment}
+              />
+            </motion.div>
           </div>
 
           <div className="w-full lg:w-1/2 pt-2 lg:p-2">
-            <img
-              alt="gallery"
-              className="block h-full w-full object-cover object-center"
-              src={villa}
-            />
+            <motion.div whileHover={{ scale: 0.9 }} className="h-full w-full">
+              <img
+                alt="gallery"
+                className="block h-full w-full object-cover object-center"
+                src={villa}
+              />
+            </motion.div>
           </div>
 
           <div className="w-full lg:w-1/2 pt-2 lg:p-2">
-            <img
-              alt="gallery"
-              className="block h-full w-full object-cover object-center"
-              src={penthouse}
-            />
+            <motion.div whileHover={{ scale: 0.9 }} className="h-full w-full">
+              <img
+                alt="gallery"
+                className="block h-full w-full object-cover object-center"
+                src={penthouse}
+              />
+            </motion.div>
           </div>
         </div>
       </div>
