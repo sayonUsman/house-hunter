a33c68f683b726af4d375db878031ccf41b40dc8
Sayon Usman <sayonusman.ma@gmail.com>
2023-08-28 13:12:11 +0300

    Added countup package in statistics section

--- src/pages/Home/Statistics/Statistics.jsx
+++ src/pages/Home/Statistics/Statistics.jsx
@@ -1,3 +1,5 @@
+import CountUp from "react-countup";
+
 const Statistics = () => {
   return (
     <>
@@ -8,28 +10,37 @@ const Statistics = () => {
       <div className="bg-[#4c545c] grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-5 py-7 sm:py-9">
         <div className="text-center">
           <p className="font-DM text-3xl 2xl:text-5xl">
-            27 <span className="text-sm">years</span>+
+            <CountUp start={1} end={27} enableScrollSpy></CountUp>
+            <span className="text-sm">years</span>+
           </p>
           <p className="text-lg md:text-xl">Experiences</p>
         </div>
 
         <div className="text-center">
-          <p className="font-DM text-3xl 2xl:text-5xl">3999+</p>
+          <p className="font-DM text-3xl 2xl:text-5xl">
+            <CountUp start={1} end={3999} enableScrollSpy></CountUp>+
+          </p>
           <p className="text-lg md:text-xl">Available Properties</p>
         </div>
 
         <div className="text-center">
-          <p className="font-DM text-3xl 2xl:text-5xl">625+</p>
+          <p className="font-DM text-3xl 2xl:text-5xl">
+            <CountUp start={1} end={625} enableScrollSpy></CountUp>+
+          </p>
           <p className="text-lg md:text-xl">Guaranteed Agents</p>
         </div>
 
         <div className="text-center">
-          <p className="font-DM text-3xl 2xl:text-5xl">7K+</p>
+          <p className="font-DM text-3xl 2xl:text-5xl">
+            <CountUp start={1} end={7} enableScrollSpy></CountUp>K+
+          </p>
           <p className="text-lg md:text-xl">Happy Clients</p>
         </div>
 
         <div className="text-center">
-          <p className="font-DM text-3xl 2xl:text-5xl">127</p>
+          <p className="font-DM text-3xl 2xl:text-5xl">
+            <CountUp start={1} end={127} enableScrollSpy></CountUp>
+          </p>
           <p className="text-lg md:text-xl">Winning Awards</p>
         </div>
       </div>
