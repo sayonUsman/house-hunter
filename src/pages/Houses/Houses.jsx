94e9e44e2797b9d5234a7c0a3fc902ae032d3233
Sayon Usman <sayonusman.ma@gmail.com>
2023-09-24 14:56:46 +0300

    Added code to implement pagination

--- src/pages/Houses/Houses.jsx
+++ src/pages/Houses/Houses.jsx
@@ -6,27 +6,34 @@ import moment from "moment/moment";
 import LoadingDots from "../../components/LoadingDots";
 import Message from "../../components/Message";
 import ErrorMessage from "../../components/ErrorMessage";
+import "./Houses.css";
 
 const Houses = () => {
   const [houses, setHouses] = useState(null);
+  const [totalHouses, setTotalHouses] = useState(0);
   const [message, setMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
   const navigate = useNavigate();
   let userInfo = localStorage.getItem("user-info");
   userInfo = JSON.parse(userInfo);
 
   useEffect(() => {
-    fetch(`https://house-hunter.cyclic.app/houses-details`)
+    fetch(
+      `https://house-hunter.cyclic.app/houses-details?skipNumber=${
+        pageNumber * 12 - 12
+      }`
+    )
       .then((res) => res.json())
       .then((data) => {
-        data.reverse();
-        setHouses(data);
+        setTotalHouses(data.length);
+        setHouses(data.houses);
       })
       .catch((error) => {
         setMessage("");
         setErrorMessage(error.message);
       });
-  }, []);
+  }, [pageNumber]);
 
   const handleViewDetails = (id) => {
     if (!userInfo) {
@@ -106,6 +113,26 @@ const Houses = () => {
     }
   };
 
+  const increasePageNumber = () => {
+    if (Math.ceil(totalHouses / 12) === pageNumber) {
+      setPageNumber(1);
+    } else {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  const decreasePageNumber = () => {
+    if (pageNumber === 1) {
+      setPageNumber(pageNumber);
+    } else {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  const handleSelectedPage = (pageNo) => {
+    setPageNumber(pageNo);
+  };
+
   return (
     <>
       <video autoPlay loop muted className="mb-12 md:mb-16 lg:mb-20">
@@ -117,46 +144,80 @@ const Houses = () => {
       </h1>
 
       {houses && (
-        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-3 xl:mx-20 2xl:mx-6 xl:pt-3 2xl:pt-5 pb-7">
-          {houses.map((house) => (
-            <div className="flex mx-auto" key={house._id}>
-              <div className="sm:w-[475px] lg:w-[445px] xl:w-[475px] card glass rounded-lg mx-4 sm:mx-7 2xl:mx-0 mt-7 sm:mt-9">
-                <figure className="p-3">
-                  <img
-                    src={house.url}
-                    alt="House Image"
-                    className="sm:h-72 rounded-lg"
-                  />
-                </figure>
+        <>
+          <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-3 xl:mx-20 2xl:mx-6 xl:pt-3 2xl:pt-5 pb-7">
+            {houses.map((house) => (
+              <div className="flex mx-auto" key={house._id}>
+                <div className="sm:w-[475px] lg:w-[445px] xl:w-[475px] card glass rounded-lg mx-4 sm:mx-7 2xl:mx-0 mt-7 sm:mt-9">
+                  <figure className="p-3">
+                    <img
+                      src={house.url}
+                      alt="House Image"
+                      className="sm:h-72 rounded-lg"
+                    />
+                  </figure>
 
-                <div className="card-body">
-                  <span>
-                    <p className="font-semibold mt-5">{`Location: ${house.address}`}</p>
-                    <p className="font-semibold">{`Room Size: ${house.roomSize}`}</p>
-                    <p className="font-semibold">{`Rent: ${house.rent} BDT`}</p>
-                    <p className="font-semibold">{`Availability Date: ${house.availabilityDate}`}</p>
+                  <div className="card-body">
+                    <span>
+                      <p className="font-semibold mt-5">{`Location: ${house.address}`}</p>
+                      <p className="font-semibold">{`Room Size: ${house.roomSize}`}</p>
+                      <p className="font-semibold">{`Rent: ${house.rent} BDT`}</p>
+                      <p className="font-semibold">{`Availability Date: ${house.availabilityDate}`}</p>
 
-                    <div className="card-actions justify-end mt-7">
-                      <button
-                        className="btn rounded-md bg-[#2a303c] text-white border border-white hover:bg-black hover:bg-opacity-70 hover:border-none"
-                        onClick={() => handleViewDetails(house._id)}
-                      >
-                        View Details
-                      </button>
+                      <div className="card-actions justify-end mt-7">
+                        <button
+                          className="btn rounded-md bg-[#2a303c] text-white border border-white hover:bg-black hover:bg-opacity-70 hover:border-none"
+                          onClick={() => handleViewDetails(house._id)}
+                        >
+                          View Details
+                        </button>
 
-                      <button
-                        className="btn rounded-md bg-[#2a303c] text-white boder border-white ml-3 hover:bg-black hover:bg-opacity-70 hover:border-none"
-                        onClick={() => handleBooking(house)}
-                      >
-                        Book Now
-                      </button>
-                    </div>
-                  </span>
+                        <button
+                          className="btn rounded-md bg-[#2a303c] text-white boder border-white ml-3 hover:bg-black hover:bg-opacity-70 hover:border-none"
+                          onClick={() => handleBooking(house)}
+                        >
+                          Book Now
+                        </button>
+                      </div>
+                    </span>
+                  </div>
                 </div>
               </div>
+            ))}
+          </div>
+
+          <div className="flex justify-center my-3 xl:my-7">
+            <div className="join">
+              <button
+                className="join-item btn bg-black text-white"
+                onClick={decreasePageNumber}
+              >
+                Previous
+              </button>
+
+              {[...Array(Math.ceil(totalHouses / 12))].map((_, i) => {
+                return (
+                  <button
+                    key={i}
+                    className={`join-item btn bg-black text-white hover:text-white ${
+                      pageNumber === i + 1 ? "active" : ""
+                    }`}
+                    onClick={() => handleSelectedPage(i + 1)}
+                  >
+                    {i + 1}
+                  </button>
+                );
+              })}
+
+              <button
+                className="join-item btn bg-black text-white"
+                onClick={increasePageNumber}
+              >
+                Next
+              </button>
             </div>
-          ))}
-        </div>
+          </div>
+        </>
       )}
 
       {!houses && <LoadingDots></LoadingDots>}
